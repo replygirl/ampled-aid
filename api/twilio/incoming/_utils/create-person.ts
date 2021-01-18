7849f8489eae5098939f105c2bfb690383434476
replygirl <moon@moon.fyi>
2021-01-18 18:00:50 -0500

    dont set name to temp

--- api/twilio/incoming/_utils/create-person.ts
+++ api/twilio/incoming/_utils/create-person.ts
@@ -3,7 +3,4 @@ import type { Person, PersonFields } from '../../../airtable/_types'
 import type { TwilioSmsMessage } from '../_types'
 
 export default async ({ from }: TwilioSmsMessage) =>
-  await createRecord<PersonFields, Person>('People', {
-    name: 'TEMP',
-    phone: from
-  })
+  await createRecord<PersonFields, Person>('People', { phone: from })
