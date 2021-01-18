a6c3b228146c06075e913ae07332c19171e06bc6
replygirl <moon@moon.fyi>
2021-01-18 16:57:36 -0500

    use twilio from number

--- api/twilio/incoming/_utils/create-message.ts
+++ api/twilio/incoming/_utils/create-message.ts
@@ -1,5 +1,5 @@
 import type { TwilioSmsMessage } from '../_types'
 import twilio from './twilio'
 
-export default ({ from }: TwilioSmsMessage, to: string, body: string) =>
+export default ({ to: from }: TwilioSmsMessage, to: string, body: string) =>
   twilio.messages.create({ body, from, to })
