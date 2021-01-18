7f98c98ac2aa5be558ee2427f2b8cadcd81e3485
replygirl <moon@moon.fyi>
2021-01-18 15:35:03 -0500

    set edit status if not editing

--- api/twilio/incoming/index.ts
+++ api/twilio/incoming/index.ts
@@ -2,7 +2,7 @@ import { camelCase } from '@replygirl/change-case-object'
 import tc from '@replygirl/tc'
 import type { NowResponse } from '@vercel/node'
 
-import type { Person } from '../../airtable/_types'
+import type { Offer, Person } from '../../airtable/_types'
 import { updatePerson } from '../../airtable/_utils'
 import type { TwilioSmsMessage, TwilioSmsIncomingRequest } from './_types'
 import {
@@ -12,18 +12,23 @@ import {
   findPerson
 } from './_utils'
 
+const setEditStatus = (
+  personId?: string,
+  offerId?: string,
+  editing?: string | null,
+) => updatePerson(personId as string, {
+  editing: editing && offerId ? [offerId as string] : [],
+  editingField: editing
+})
+
 const setOffer = async (
   msg: TwilioSmsMessage,
   personId?: string,
   offerId?: string,
   field?: string | null
 ) => {
-  const { editing, offer } = await editOffer(msg, offerId as string, field)
-  console.log(editing, offer)
-  await updatePerson(personId as string, {
-    editing: editing ? [offerId as string] : [],
-    editingField: editing
-  })
+  const { editing } = await editOffer(msg, offerId as string, field)
+  return setEditStatus(personId, offerId, editing)
 }
 
 export default async (req: TwilioSmsIncomingRequest, res: NowResponse) => (
@@ -47,6 +52,7 @@ export default async (req: TwilioSmsIncomingRequest, res: NowResponse) => (
           break
         default:
           if (editingId) await setOffer(msg, personId, editingId, editingField)
+          else await setEditStatus(personId, offerId, null)
       }
 
       return res.status(200)
